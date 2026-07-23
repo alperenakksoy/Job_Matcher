1fd2ca240353bb31a133eb6043e3a88327192297
Ahmet Alperen Aksoy <alperenaksoy611@gmail.com>
2026-07-23 15:53:04 +0200

    Start a project and add a ThreadLocal

---
+++ src/main/java/com/example/job_matchwer/JobMatchwerApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_matchwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatchwerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobMatchwerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/job_matchwer/JobMatchwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_matchwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchwerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
